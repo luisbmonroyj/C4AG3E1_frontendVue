b21b996431a32aff1d6629c8f720460d29df902e
luisbmonroyj <lbmonroyj@gmail.com>
2022-10-02 09:20:17 -0500

    login y signup

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,12 +1,23 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
+import LogIn from './components/LogIn.vue'
+import SignUp from './components/SignUp.vue'
 
 const routes = [
   {
     path: '/',
     name: 'root',
     component: App
-  }]
+  },
+  {
+  path: '/user/logIn',
+  name: "logIn",
+  component: LogIn
+  },
+  {
+  path: '/user/signUp',
+  name: "signUp",
+  component: SignUp}]
 
 const router = createRouter({
   history: createWebHashHistory(),
