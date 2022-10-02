74c31b948d3785ae91448990d5574626a6be3fe5
luisbmonroyj <lbmonroyj@gmail.com>
2022-10-02 09:06:31 -0500

    Modif plantilla

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,39 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
-</template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  <div id="app" class="app">
+  <div class="header">
+  <h1>Plantilla por defecto</h1>
+  </div>
+  </div>
+  </template>
+  <script>
+  export default {
+  name: 'App',
+  data: function(){
+  },
+  methods:{
+  },
+  created: function(){
+  }
+  }
+  </script>
+  <style>
+  body{
+  margin: 0 0 0 0;
+  }
+  .header{
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 10vh;
+  min-height: 100px;
+  background-color: #283747 ;
+  color:#E5E7E9 ;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  }
+  .header h1{
+  width: 20%;
   text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  }
+  </style>
